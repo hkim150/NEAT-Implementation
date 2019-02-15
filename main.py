04a3757e4010879f0383451f44e18d31ee88b43b
Hyun Bin Kim <hkim0150@gmail.com>
2019-02-14 21:42:09 -0800

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from game import *
 
 def main():
     initial_organism_count = 10
-    max_generations = 100
+    max_generations = 50
 
     game = Game()
     gen = Generation()
@@ -16,19 +16,23 @@ def main():
         gen.add_organism(Organism(new_genome))
 
     for _ in range(max_generations):
-        print("generation: " + str(Generation.nextGenerationNumber) + ", population: " + str(gen.population))
-        print("num species: " + str(len(gen.species)))
         show = False
-        if Generation.nextGenerationNumber > max_generations * 0.0:
-            if not show:
-                game.win = pg.display.set_mode((Game.SCREEN_WIDTH, Game.SCREEN_HEIGHT))
-                show = True
+        if gen.generation_number > max_generations * 0.8:
+            game.win = pg.display.set_mode((Game.SCREEN_WIDTH, Game.SCREEN_HEIGHT))
+            show = True
+        s_num = 0
         for s in gen.species:
+            s_num += 1
+            o_num = 0
             for organism in s.organisms:
-                print("num_nodes: " + str(len(organism.genome.nodes)) + " num_connections: " + str(len(organism.genome.connections)))
+                o_num += 1
+                print("generation: " + str(gen.generation_number) + ", population: " + str(
+                    gen.population) + ", num species: " + str(len(gen.species)) + ", species: " + str(
+                    s_num) + ", organism: " + str(o_num))
+                #print("num_nodes: " + str(len(organism.genome.nodes)) + " num_connections: " + str(len(organism.genome.connections)))
                 game.game_start(organism, show)
 
-        gen = gen.get_next_generation()
+        gen.next_generation()
 
 
 if __name__ == '__main__': main()
