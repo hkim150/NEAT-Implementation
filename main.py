0c5b02cc6a10477c8357c3d3f409dc47bdda004e
Hyun Bin Kim <hkim0150@gmail.com>
2019-02-17 18:31:54 -0800

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,16 @@ from game import *
 
 
 def main():
-    initial_organism_count = 10
-    max_generations = 50
+    user_mode = False
 
+    window = pg.display.set_mode((Game.window_width, Game.window_height))
     game = Game()
+
+    initial_organism_count = 10
+    max_generations = 200
+
     gen = Generation()
-    initial_genome = Genome(Game.NUM_INPUT, Game.NUM_ACTIONS)
+    initial_genome = Genome(Game.num_inputs, Game.num_actions)
     initial_genome.add_connection_mutation()
 
     for _ in range(initial_organism_count):
@@ -15,18 +19,13 @@ def main():
         new_genome.mutate()
         gen.add_organism(Organism(new_genome))
 
-    show = False
     for curr_g in range(max_generations):
         pop = gen.population
         num_sp = len(gen.species)
-        if gen.generation_number > max_generations * 0.0:
-            game.win = pg.display.set_mode((Game.SCREEN_WIDTH, Game.SCREEN_HEIGHT))
-            show = True
-        for curr_s, s in enumerate(gen.species):
-            for curr_o, organism in enumerate(s.organisms):
-                info = [curr_g + 1, pop, num_sp, curr_s + 1, curr_o + 1]
-                info.extend(organism.genome.get_genome_info())
-                game.game_start(organism, show, info)
+        organisms = gen.get_all_organisms()
+        info = [curr_g + 1, pop, num_sp]
+        #info.extend(organism.genome.get_genome_info())
+        game.game_start(window, organisms, info, user_mode)
 
         gen.next_generation()
 
